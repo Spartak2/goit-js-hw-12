715b8a522de360b535ecead4afcb7f8e9b338959
Spartak2 <orkotop030@gmail.com>
2024-04-18 00:30:44 +0300

    1

---
+++ src/js/pixaday.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export async function fetchImages(searchWord, currPage) {
+    const loader = document.querySelector('.loader');
+    const KEY = '43280076-efaf032a147c4a401dc5ab87e';
+    const BASE_URI = 'https://pixabay.com/api/';
+    const resp = await axios.get(URL, {
+        params: {
+          key: KEY,
+          q: searchWord,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: true,
+          page: currPage,
+          per_page: 15,
+        },
+    });
+    return resp.data;
+}
