45f542ed5dd57c36f82ef834093757a33d698ebe
Spartak2 <orkotop030@gmail.com>
2024-04-19 17:10:04 +0300

    4

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import { fetchImages  } from './js/pixaday-api';
 
 import {
   renderMarcup,
